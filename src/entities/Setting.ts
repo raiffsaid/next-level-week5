579d1630500aa258840d49f7f4b38b8fb574942a
Raiff Smith Said <raiff.said@gmail.com>
2021-04-22 18:28:20 -0300

    feat: created entities and migrations

---
+++ src/entities/Setting.ts
@@ -0,0 +1,28 @@
+import { Entity, Column, CreateDateColumn, UpdateDateColumn, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from "uuid"; // Renomeando v4 para uuid
+
+@Entity("settings")
+class Setting {
+    @PrimaryColumn()
+    id: String;
+
+    @Column()
+    username: String;
+
+    @Column()
+    chat: boolean;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuid();
+        }
+    }
+}
+
+export { Setting };
